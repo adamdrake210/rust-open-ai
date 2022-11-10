423efcdc9d59309c396d6c6dbd4d00ed0fa9c80e
Adam Drake <adam@chemify.io>
2022-11-10 11:23:12 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use hyper::body::Buf;
+use hyper::{Body, Client, Request, header};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Deserialize, Serialize};
+use spinners::{Spinner, Spinners};
+use std::env;
+use std::io::{stdin, stdout, Write};
+
+#[derive(Deserialize, Debug)]
+struct OAIChoices {
+  text: String,
+  index: u8,
+  logprobs: Option<u8>,
+  finish_reason: String
+}
+
+#[derive(Deserialize, Debug)]
+struct OAIResponse {
+  id: Option<String>,
+  object: Option<String>,
+  created: Option<u64>,
+  model: Option<String>,
+  choices: Vec<OAIChoices>
+}
+
+#[derive(Serialize, Debug)]
+struct OAIRequest {
+  prompt: String,
+  max_tokens: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+  let https = HttpsConnector::new();
+  let client = Client::builder().build(https);
+  let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
+
+  let preamble = "Answer the following question accurately, but make it very extreme.";
+
+  let oai_token: String = env::var("OAI_TOKEN").unwrap();
+  let auth_header_val = format!("Bearer {}", oai_token);
+
+  println!("{esc}c", esc = 27 as char);
+
+  loop {
+      print!(">> ");
+      stdout().flush().unwrap();
+      let mut user_text = String::new();
+
+      stdin().read_line(&mut user_text)
+            .expect("Failed to read line");
+
+      println!("");
+
+      let sp = Spinner::new(&Spinners::Dots12, "Thinking...".to_string());
+
+      let oai_request = OAIRequest {
+        prompt: format!("{} {}", preamble, user_text),
+        max_tokens: 64
+      };
+
+      let body = Body::from(serde_json::to_vec(&oai_request)?);
+
+      let req = Request::post(uri)
+        .header(header::CONTENT_TYPE, "application/json")
+        .header("Authorization", &auth_header_val)
+        .body(body)
+        .unwrap();
+
+      let res = client.request(req).await?;
+
+      let body = hyper::body::aggregate(res).await?;
+
+      let json: OAIResponse = serde_json::from_reader(body.reader())?;
+
+      sp.stop();
+
+      println!("");
+
+      println!("{}", json.choices[0].text);
+
+      }
+}
+
+  
+
